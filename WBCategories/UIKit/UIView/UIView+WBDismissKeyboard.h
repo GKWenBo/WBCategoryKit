df61870dd852e4b3b89a29abd6509f348a6e6e7d
wenbo_company <1050794513@qq.com>
2018-02-10 15:37:26 +0800

    添加隐藏键盘方法

---
+++ WBCategories/UIKit/UIView/UIView+WBDismissKeyboard.h
@@ -0,0 +1,13 @@
+//
+//  UIView+WBDismissKeyboard.h
+//  WBCategories
+//
+//  Created by Admin on 2018/2/10.
+//  Copyright © 2018年 WENBO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (WBDismissKeyboard)
+
+@end
