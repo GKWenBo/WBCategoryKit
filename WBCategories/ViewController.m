06cfc13ab3063669dc057fce5f2fcc5ca9cc5546
wenbo <bo.wen@cyzone.cn>
2018-05-17 18:35:58 +0800

    增加时间格式缓存工具，修改相关方法

--- WBCategories/ViewController.m
+++ WBCategories/ViewController.m
@@ -12,11 +12,16 @@
 #import "WBMacroDefinition.h"
 #import "NSString+WBPredicate.h"
 
+#import <libextobjc/EXTScope.h>
 
 @interface ViewController ()
 {
     UIView *redView;
 }
+
+@property (nonatomic, copy) void(^TestBlock)(NSString *test);
+
+
 @end
 
 @implementation ViewController
@@ -25,21 +30,21 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     // 测试数组
-    NSArray *arr = @[@"1",@"2",@"2",@"2",@"2",@"2",@"2",@"2",@"2"];
-    NSMutableArray *tableArray = [[NSMutableArray alloc] initWithArray:arr];
-    NSLog(@"arr====%@   tableArray====%@",arr[100],tableArray[100]);
-    NSLog(@"arr====%@   tableArray====%@",[arr objectAtIndex:100],tableArray[100]);
+//    NSArray *arr = @[@"1",@"2",@"2",@"2",@"2",@"2",@"2",@"2",@"2"];
+//    NSMutableArray *tableArray = [[NSMutableArray alloc] initWithArray:arr];
+//    NSLog(@"arr====%@   tableArray====%@",arr[100],tableArray[100]);
+//    NSLog(@"arr====%@   tableArray====%@",[arr objectAtIndex:100],tableArray[100]);
+//
+//    // 测试字典
+//    NSDictionary *dict = @{@"name":@"",@"age":@"20"};
+//    NSMutableDictionary *tableDict = [[NSMutableDictionary alloc] initWithDictionary:dict];
+//    NSLog(@"dict---name====%@   tableDict---age====%@",[dict objectForKey:@"name"],[tableDict objectForKey:@"age"]);
+//    NSLog(@"dict---name====%@   tableDict---age====%@",[dict objectForKey:@"name"],[tableDict objectForKey:@"age"]);
+//
+////    // 测试字符串
+//    NSMutableString *tableString = [[NSMutableString alloc] initWithFormat:@"防止项目数组字典越界崩溃"];
+//    NSLog(@"%@",[tableString substringFromIndex:100]);
     
-    // 测试字典
-    NSDictionary *dict = @{@"name":@"",@"age":@"20"};
-    NSMutableDictionary *tableDict = [[NSMutableDictionary alloc] initWithDictionary:dict];
-    NSLog(@"dict---name====%@   tableDict---age====%@",[dict objectForKey:@"name"],[tableDict objectForKey:@"age"]);
-    NSLog(@"dict---name====%@   tableDict---age====%@",[dict objectForKey:@"name"],[tableDict objectForKey:@"age"]);
-    
-//    // 测试字符串
-    NSMutableString *tableString = [[NSMutableString alloc] initWithFormat:@"防止项目数组字典越界崩溃"];
-    NSLog(@"%@",[tableString substringFromIndex:100]);
-
 }
 
 
