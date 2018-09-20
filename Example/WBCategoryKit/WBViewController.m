7cfad2338e78985550d0bf7f2f0c17e5c20273eb
wenbo <1050794513@qq.com>
2018-09-20 11:33:26 +0800

    添加iPhone XS，iPhone XR，iPhone XS Max适配宏定义

--- Example/WBCategoryKit/WBViewController.m
+++ Example/WBCategoryKit/WBViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "WBViewController.h"
+#import <WBCategoryKit/WBMacro.h>
 
 @interface WBViewController ()
 
@@ -17,7 +18,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
 }
 
 - (void)didReceiveMemoryWarning
