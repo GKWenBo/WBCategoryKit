7c0e73b40270a60743dede4a1561e9233f67eb63
wenbo <1050794513@qq.com>
2018-09-25 09:59:28 +0800

    delete

--- Example/Pods/Target Support Files/Pods-WBCategoryKit_Tests/Pods-WBCategoryKit_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WBCategoryKit_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_WBCategoryKit_Tests
-@end
--- Example/Pods/Target Support Files/WBCategoryKit/WBCategoryKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_WBCategoryKit : NSObject
-@end
-@implementation PodsDummy_WBCategoryKit
-@end
